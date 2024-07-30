3232935551c83604dff5b3e4510766379edecb45
miiiwdy <fahmiwidyaa@gmail.com>
2024-07-30 21:33:57 +0700

    upd

---
+++ growtopia/server_data.php
@@ -0,0 +1,13 @@
+server|37.221.94.136
+port|17777
+loginurl|login.growtopia.com.tr
+type|1
+#maint|Server is currently initializing or re-syncing with sub servers. Please try again in a minute.
+beta_server|127.0.0.1
+beta_port|17091
+beta_type|1
+beta2_server|127.0.0.1
+beta2_port|17099
+beta2_type|1
+meta|
+RTENDMARKERBS1001
